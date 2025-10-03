6f681d58892b3c965b701a9a388beba06ca34f4d
ssatyamm07 <satyamraj151.rajgir@gmail.com>
2025-10-03 21:46:19 +0530

    fixed login and register

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,33 @@
+// import { Router } from "express";
+// import verticalRoutes from './vertical.route';
+// import TemplateRoutes from './template.route';
+// import assetRoutes from './asset.route';
+// import campaignRoutes from './campaign.route';
+// import authRoutes from './auth.route';
+// const router = Router();
+
+// router.use('/verticals', verticalRoutes);
+// router.use('/templates', TemplateRoutes);
+// router.use('/assets', assetRoutes);
+// router.use('/campaigns', campaignRoutes);
+// router.use('/auth', authRoutes);
+
+// export default router;
+
 import { Router } from "express";
 import verticalRoutes from './vertical.route';
 import TemplateRoutes from './template.route';
 import assetRoutes from './asset.route';
 import campaignRoutes from './campaign.route';
-import authRoutes from './auth.route';
+//import { authMiddleware } from '../middlewares/auth.middleware';
+import authRouter from "./auth.route";
 const router = Router();
 
-router.use('/verticals', verticalRoutes);
+
+router.use('/verticals',  verticalRoutes);
 router.use('/templates', TemplateRoutes);
 router.use('/assets', assetRoutes);
 router.use('/campaigns', campaignRoutes);
-router.use('/auth', authRoutes);
+router.use('/auth', authRouter);
 
 export default router;
