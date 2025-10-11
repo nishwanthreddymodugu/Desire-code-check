e681bb052f5fe597cd7cd77768692310fd972601
nishwanthreddy <nishwanthreddymodugu@gmail.com>
2025-10-11 21:03:34 +0530

    Upload Asset Image API

--- src/routes/campaign.route.ts
+++ src/routes/campaign.route.ts
@@ -187,5 +187,43 @@ router.get('/:campaignId/images/:imageName', async (req, res) => {
   }
 });
 
+/* ---------------- UPLOAD ASSET IMAGE ROUTE ---------------- */
+router.post(
+  '/image/asset/upload',
+  imageUpload.single('image'),
+  async (req: Request, res: Response) => {
+    const { campaignId, assetId, requestId } = req.body;
+    const file = req.file;
+
+    if (!campaignId || !assetId || !requestId) {
+      logger.error('campaignId, assetId, and requestId are required');
+      return res.status(400).json({ message: 'campaignId, assetId, and requestId are required' });
+    }
+
+    if (!file) {
+      logger.error('Image file missing');
+      return res.status(400).json({ message: 'Image file is required' });
+    }
+
+    try {
+      const result = await CampaignService.uploadAssetImage(
+        Number(campaignId),
+        Number(assetId),
+        Number(requestId),
+        file
+      );
+
+      res.status(200).json({
+        message: 'Asset image uploaded successfully',
+        ...result,
+       // s3Url: `https://${process.env.IMAGE_BUCKET}.s3.amazonaws.com/${result.s3Key}`,
+      });
+    } catch (err: unknown) {
+      const message = err instanceof Error ? err.message : 'Internal Server Error';
+      logger.error(message);
+      res.status(500).json({ error: message });
+    }
+  }
+);
 
 export default router;
