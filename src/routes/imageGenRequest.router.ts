908f52946a371c19b4707f17ad3736c40f2eb305
ssatyamm07 <satyamraj151.rajgir@gmail.com>
2025-10-13 15:52:49 +0530

    fixed campaignId

--- src/routes/imageGenRequest.router.ts
+++ src/routes/imageGenRequest.router.ts
@@ -114,7 +114,7 @@ router.get('/:campaignId/image/requests/:requestId/get', async (req: Request, re
     return res.status(200).json({
       id: row.id,
       prompt: row.prompt,
-      camapignId: row.campaignId,
+      campaignId: row.campaignId,
       verticalId: row.verticalId,
       templateId: row.templateId,
       // projectId: row.figmaProjectId ?? null,
